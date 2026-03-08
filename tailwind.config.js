bc2b444450c5a3e5d59f20458079a472bb2ebace
AnshGupta2506 <158730074+AnshGupta2506@users.noreply.github.com>
2026-03-08 17:58:36 +0530

    Deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./**/*.html"],
+  content: ["./*.html", "./assets/js/*.js"],
   safelist: ["active"],
   theme: {
     fontFamily: {
